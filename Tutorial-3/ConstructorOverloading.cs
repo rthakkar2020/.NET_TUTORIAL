68742bf91bba2e1c95872df68ce1a0eee0e96dff
rthakkar2020 <49087120+rthakkar2020@users.noreply.github.com>
2019-03-30 11:01:18 +0530

    Add files via upload

---
+++ Tutorial-3/ConstructorOverloading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p2
+{
+    public class Student
+    {
+        string name, enrollment, branch;
+        public Student(string name) {
+            this.name = name;
+        }
+        public Student(string name, string enrollment) {
+            this.name = name;
+            this.enrollment = enrollment;
+        }
+        public Student(string name, string enrollment, string branch) {
+            this.name = name;
+            this.enrollment = enrollment;
+            this.branch = branch;
+        }
+        public static void Main(String[] ar) {
+            Student s1 = new Student("Parinay");
+            Student s2 = new Student("Parinay","170473107009");
+            Student s3 = new Student("Parinay","170473107009","CS");
+        }
+    }
+}
