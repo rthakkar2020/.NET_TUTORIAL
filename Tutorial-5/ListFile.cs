23f94d635eaed9af887e1313fcc43f18b8014ce4
rthakkar2020 <49087120+rthakkar2020@users.noreply.github.com>
2019-03-30 11:02:06 +0530

    Add files via upload

---
+++ Tutorial-5/ListFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace p2
+{
+    class ListFile
+    {
+        public static void Main() {
+            string[] Directories = Directory.GetDirectories(@"VS");
+            foreach (string dir in Directories)
+                Console.WriteLine(dir);
+            string[] files = Directory.GetFiles(@"VS");
+            foreach (string file in files)
+                Console.WriteLine(file);
+
+            Console.ReadKey();
+
+        }
+    }
+}
